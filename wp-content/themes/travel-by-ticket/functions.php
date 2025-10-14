265c355f24bc63d8b8f898f49d48b1505bfba495
jamatija <matijatijanic@gmx.com>
2025-10-14 14:40:20 +0200

    qfix form initialization layout shift

--- wp-content/themes/travel-by-ticket/functions.php
+++ wp-content/themes/travel-by-ticket/functions.php
@@ -3,12 +3,26 @@ function enqueue_bus_search_scripts2() {
     if (is_page() || is_single()) {
         global $post;
         if (has_shortcode($post->post_content, 'bus_search_form')) {
+
             // Select2 CSS
             wp_enqueue_style(
                 'select2',
                 'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css',
                 array(),
                 '4.1.0'
+            );
+
+                        // Flatpickr CSS
+            wp_enqueue_style(
+                'flatpickr',
+                'https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css',
+                array(),
+                '4.6.13'
+            );
+
+             wp_enqueue_style(
+                'bus-search-form', 
+                get_stylesheet_directory_uri() . '/assets/css/bus-search.css', ['select2','flatpickr'], null
             );
             
             // Select2 JS
@@ -19,28 +33,8 @@ function enqueue_bus_search_scripts2() {
                 '4.1.0',
                 true
             );
-            
-            // Tvoj custom JS
-            wp_enqueue_script(
-                'bus-search-form', 
-                get_stylesheet_directory_uri() . '/assets/js/bus-search.js', 
-                array('jquery', 'select2'), // Zavisi od Select2
-                '1.0.1', 
-                true
-            );
-            
-            wp_enqueue_style(
-                'bus-search-form', 
-                get_stylesheet_directory_uri() . '/assets/css/bus-search.css'
-            );
-            
-            wp_localize_script('bus-search-form', 'busSearchConfig', array(
-                'apiUrl' => rest_url('busticket/v1/cities/'),
-                'nonce' => wp_create_nonce('wp_rest'),
-                'lang' => 'MNE'
-            ));
 
-               // Flatpickr JS
+              // Flatpickr JS
             wp_enqueue_script(
                 'flatpickr',
                 'https://cdn.jsdelivr.net/npm/flatpickr',
@@ -49,14 +43,20 @@ function enqueue_bus_search_scripts2() {
                 true
             );
 
-
-            // Flatpickr CSS
-            wp_enqueue_style(
-                'flatpickr',
-                'https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css',
-                array(),
-                '4.6.13'
+            wp_enqueue_script(
+                'bus-search-form', 
+                get_stylesheet_directory_uri() . '/assets/js/bus-search.js', 
+                array('jquery', 'select2'), 
+                '1.0.1', 
+                true
             );
+            
+            wp_localize_script('bus-search-form', 'busSearchConfig', array(
+                'apiUrl' => rest_url('busticket/v1/cities/'),
+                'nonce' => wp_create_nonce('wp_rest'),
+                'lang' => 'MNE'
+            ));
+
         }
     }
 }
